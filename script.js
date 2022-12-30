2208b4f31ef3097abbf8e93b8efa6204815b9ffc
Madeline0421 <madelinehou421@gmail.com>
2022-12-30 01:45:40 -0500

    Add ability to increase and decrease number of pixels in grid

--- script.js
+++ script.js
@@ -5,23 +5,54 @@
 
 const gridContainer = document.querySelector('.grid-container');
 
-let size = 256; 
+{/* <div id="value">16 x 16</div>
+<input type="range" id="grid-input" min="16" max ="50" step="1"> */}
 
-for (let i = 0; i < size; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-        gridContainer.append(gridItem);
-        
-        gridItem.addEventListener('mousedown', () => {
-            allItems.forEach(item => { 
-                item.addEventListener('mouseover', changeColor)
+const value = document.querySelector('#value');
+const gridInput = document.querySelector('#grid-input'); 
+
+value.textContent = gridInput.value;
+
+
+gridInput.addEventListener('input', (e) => {
+    value.textContent = e.target.value;
+    console.log(typeof (value.textContent));
+    rowSize = parseInt(e.target.value);
+    size = rowSize * rowSize;  
+    createGrid();
+    console.log("size: " + rowSize);
+});
+
+function reset() {
+    document.querySelectorAll(".grid-item").forEach((e) => e.parentNode.removeChild(e));
+  }
+
+rowSize = 16;
+size = 256;
+createGrid();
+
+function createGrid() {
+    reset();
+
+    for (let i = 0; i < size; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.className = 'grid-item';
+            gridContainer.append(gridItem);
+            
+            gridItem.addEventListener('click', () => {
+                allItems.forEach(item => { 
+                    item.addEventListener('mouseover', changeColor)
+                });
             });
-        });
+    }
+    gridContainer.setAttribute('style',
+    // updating the attribute with the following concatenated string:
+    'grid-template-columns: repeat(' + rowSize + ', 1fr)');
+
 }
 const allItems = document.querySelectorAll('.grid-item');
 
 function changeColor(e) { 
     e.target.classList.add('change-color');
 }
-
 
