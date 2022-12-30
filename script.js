6126932192de93927fdd90e01a33dbc112e69c47
Madeline0421 <madelinehou421@gmail.com>
2022-12-29 23:25:08 -0500

    Change color of pixel when hovered over

--- script.js
+++ script.js
@@ -5,15 +5,23 @@
 
 const gridContainer = document.querySelector('.grid-container');
 
+let size = 256; 
 
-for (let i = 0; i < 16; i++) { 
-    for (let j = 0; j < 16; j++) {
+for (let i = 0; i < size; i++) {
         const gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
         gridContainer.append(gridItem);
-    }
+        
+        gridItem.addEventListener('mousedown', () => {
+            allItems.forEach(item => { 
+                item.addEventListener('mouseover', changeColor)
+            });
+        });
+}
+const allItems = document.querySelectorAll('.grid-item');
+
+function changeColor(e) { 
+    e.target.classList.add('change-color');
 }
 
-// const gridItem = document.createElement('div');
-// gridItem.className = 'grid-item';
-// gridContainer.append(gridItem);
+
