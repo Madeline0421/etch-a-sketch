7ec015f363c6529bb9894e5d225716def720bc1c
Madeline0421 <madelinehou421@gmail.com>
2022-12-30 03:05:08 -0500

    Simplify event listener for each grid item

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-/**
- * Create a webpage with 16 x 16 divs. 
- * Use for loop? 
- */
-
 const gridContainer = document.querySelector('.grid-container');
-
-{/* <div id="value">16 x 16</div>
-<input type="range" id="grid-input" min="16" max ="50" step="1"> */}
-
 const value = document.querySelector('#value');
 const gridInput = document.querySelector('#grid-input'); 
 
-value.textContent = gridInput.value;
+value.textContent = "Grid Size: " + gridInput.value;
 
-
+/**
+ * Updates size of the grid
+ */
 gridInput.addEventListener('input', (e) => {
-    value.textContent = e.target.value;
+    value.textContent = "Grid Size: " + e.target.value;
     console.log(typeof (value.textContent));
     rowSize = parseInt(e.target.value);
     size = rowSize * rowSize;  
@@ -33,19 +26,14 @@ createGrid();
 
 function createGrid() {
     reset();
-
     for (let i = 0; i < size; i++) {
-            const gridItem = document.createElement('div');
-            gridItem.className = 'grid-item';
-            gridContainer.append(gridItem);
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridContainer.append(gridItem);
+        gridItem.addEventListener('mouseover', changeColor);
     }
-    const allItems = document.querySelectorAll('.grid-item');
 
-    allItems.forEach(item => { 
-        item.addEventListener('mouseover', changeColor)
-    });
     gridContainer.setAttribute('style',
-    // updating the attribute with the following concatenated string:
     'grid-template-columns: repeat(' + rowSize + ', 1fr)');
 
 }
